86c5a0bfb58dfce6c670ca2c3bea0571874cc4e1
DAbrigo <di.abrigo@duocuc.cl>
2025-05-06 19:09:30 -0400

    hola mi gente tamo repasando, arreglando la primera cagada parte 3

--- app.sh
+++ app.sh
@@ -3,7 +3,7 @@ echo "RUN pip install flask" >> Dockerfile
 echo "COPY index.html /home/myapp/templates/" >> Dockerfile 
 echo "COPY app.py /home/myapp/" >> Dockerfile 
 echo "EXPOSE 9000" >> Dockerfile 
-echo "CMD python3 /home/myapp/sample_app.py" >> Dockerfile
+echo "CMD python3 /home/myapp/app.py" >> Dockerfile
 docker build -t web2 .
 docker run -d -p 9000:9000 --name web2 web2
 docker ps 
